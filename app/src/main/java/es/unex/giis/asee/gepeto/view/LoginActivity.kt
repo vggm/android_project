ec415fb09f865cf8aa460a0b86ccd38c26d8a31c
COLOSO70 <ppoly938@gmail.com>
2023-11-23 00:47:55 +0100

    SCRUM-256 INICIAR SESIÓN

--- app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
@@ -1,41 +1,23 @@
 package es.unex.giis.asee.gepeto.view
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.PreferenceManager
+import androidx.room.Room
+import es.unex.giis.asee.gepeto.database.GepetoDatabase
 
 import es.unex.giis.asee.gepeto.databinding.ActivityLoginBinding
-import es.unex.giis.asee.gepeto.model.User
 import es.unex.giis.asee.gepeto.utils.CredentialCheck
-import es.unex.giis.asee.gepeto.view.home.HomeActivity
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-    private val responseLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                with(result.data) {
-                    val name = this?.getStringExtra(JoinActivity.USERNAME).orEmpty()
-                    val password = this?.getStringExtra(JoinActivity.PASS).orEmpty()
+    private lateinit var db: GepetoDatabase
 
-                    with(binding) {
-                        etPassword.setText(password)
-                        etUsername.setText(name)
-                    }
-
-                    Toast.makeText(
-                        this@LoginActivity,
-                        "New user ($name/$password) created",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,51 +26,63 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = GepetoDatabase.getInstance(applicationContext)!!
+
         //views initialization and listeners
-        setUpUI()
         setUpListeners()
+
+        readSettings()
     }
 
-    private fun setUpUI() {
-        //get attributes from xml using binding
+    private fun readSettings(){
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this).all
+
+        val rememberMe = preferences["rememberme"] as Boolean? ?: false
+        val username = preferences["username"] as String? ?: ""
+        val password = preferences["password"] as String? ?: ""
+
+        if (rememberMe) {
+            binding.etUsername.setText(username)
+            binding.etPassword.setText(password)
+        }
     }
 
     private fun setUpListeners() {
         with(binding) {
 
             btLogin.setOnClickListener {
-                val check = CredentialCheck.login(etUsername.text.toString(), etPassword.text.toString())
-
-                if (check.fail) notifyInvalidCredentials(check.msg)
-                else navigateToHomeActivity(User(etUsername.text.toString(), etPassword.text.toString()), check.msg)
+                correctLogin()
             }
 
-            btRegister.setOnClickListener {
-                navigateToJoin()
-            }
-
-            btWebsiteLink.setOnClickListener {
-                navigateToWebsite()
-            }
         }
     }
 
-    private fun navigateToHomeActivity(user: User, msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-        HomeActivity.start(this, user)
+    private fun correctLogin(){
+        val credentialCheck = CredentialCheck.login(binding.etUsername.text.toString(), binding.etPassword.text.toString())
+
+        if (credentialCheck.fail) {
+            notifyInvalidCredentials(credentialCheck.msg)
+            return
+        }
+
+        lifecycleScope.launch{
+            val user =
+                db.userDao().findByName(binding.etUsername.text.toString()) ?: null
+            if (user != null) {
+                // db.userDao().insert(User(-1, etUsername.text.toString(), etPassword.text.toString()))
+                val passwordCheck = CredentialCheck.passwordOk(binding.etPassword.text.toString(), user.password)
+                if (passwordCheck.fail) notifyInvalidCredentials(passwordCheck.msg)
+
+            }
+            else notifyInvalidCredentials("Invalid username")
+        }
     }
 
-    private fun navigateToJoin() {
-        JoinActivity.start(this, responseLauncher)
-    }
-
-    private fun navigateToWebsite() {
-        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://trakt.tv/"))
-        startActivity(webIntent)
-    }
 
     private fun notifyInvalidCredentials(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
+
+
 
 }
